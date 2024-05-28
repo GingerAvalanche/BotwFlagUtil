b623734fe18a9fbad2c4b7e4198a0273e6631d51
Ginger Chody <chodness@gmail.com>
2024-05-28 16:13:42 -0700

    Move DataContext setter into ViewModel ctor

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,7 +1,6 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using BotwFlagUtil.ViewModels;
 using BotwFlagUtil.Views;
 
 namespace BotwFlagUtil;
@@ -17,10 +16,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel(),
-            };
+            desktop.MainWindow = new MainWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
