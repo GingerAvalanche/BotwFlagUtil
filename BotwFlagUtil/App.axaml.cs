e2136a4b6ac1daf9e85fb4c52d56941c521e7311
GingerAvalanche <chodness@gmail.com>
2024-05-28 22:19:43 -0700

    Initial working build

--- BotwFlagUtil/App.axaml.cs
+++ BotwFlagUtil/App.axaml.cs
@@ -17,6 +17,10 @@ public partial class App : Application
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
+
+#if DEBUG
+            desktop.MainWindow.AttachDevTools();
+#endif
         }
 
         base.OnFrameworkInitializationCompleted();
--- BotwFlagUtil/Views/MainWindow.axaml.cs
+++ BotwFlagUtil/Views/MainWindow.axaml.cs
@@ -1,6 +1,13 @@
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Interactivity;
+using Avalonia.Platform;
 using BotwFlagUtil.ViewModels;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Dto;
+using MsBox.Avalonia.Enums;
+using System;
+using System.IO;
 
 namespace BotwFlagUtil.Views;
 
@@ -12,18 +19,57 @@ public partial class MainWindow : Window
 
         DataContext = new MainWindowViewModel();
 
+        About.Click += About_Click;
         Exit.Click += Exit_Click;
         Export.Click += Export_Click;
         Import.Click += Import_Click;
+        Open.Click += Open_ClickAsync;
         Save.Click += Save_Click;
-        //Settings.Click += Settings_Click;
+        Settings.Click += Settings_Click;
     }
 
-    private void Exit_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private async void About_Click(object? sender, RoutedEventArgs e)
+    {
+        string message;
+        WindowIcon icon;
+        using (var streamReader = new StreamReader(AssetLoader.Open(new("avares://BotwFlagUtil/Assets/about.md"))))
+        {
+            message = streamReader.ReadToEnd();
+        }
+        using (var streamReader = AssetLoader.Open(new("avares://BotwFlagUtil/Assets/icon.png")))
+        {
+            icon = new(streamReader);
+        }
+        await MessageBoxManager.GetMessageBoxStandard(
+            new MessageBoxStandardParams()
+            {
+                ButtonDefinitions = ButtonEnum.Ok,
+                ContentTitle = "About",
+                ContentMessage = message,
+                Markdown = true,
+                MaxHeight = 800,
+                Width = 500,
+                WindowIcon = icon,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+            }).ShowWindowDialogAsync(this);
+    }
+
+    private void Exit_Click(object? sender, RoutedEventArgs e)
     {
         if ((DataContext as MainWindowViewModel)!.NeedsSave)
         {
-            //MessageBoxResult result = MessageBox;
+            var result = MessageBoxManager.GetMessageBoxStandard(
+                new MessageBoxStandardParams()
+                {
+                    ButtonDefinitions = ButtonEnum.YesNo,
+                    ContentTitle = "Discard Changes",
+                    ContentMessage = "You are about to discard unsaved changes. Continue?",
+                    WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                }).ShowWindowDialogAsync(this);
+            if (result.Result == ButtonResult.No)
+            {
+                return;
+            }
         }
         if (Avalonia.Application.Current!.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
         {
@@ -31,18 +77,58 @@ public partial class MainWindow : Window
         }
     }
 
-    private void Export_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void Export_Click(object? sender, RoutedEventArgs e)
     {
         (DataContext as MainWindowViewModel)!.Export();
     }
 
-    private void Import_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void Import_Click(object? sender, RoutedEventArgs e)
     {
         (DataContext as MainWindowViewModel)!.Import();
     }
 
-    private void Save_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private async void Open_ClickAsync(object? sender, RoutedEventArgs e)
+    {
+        if ((DataContext as MainWindowViewModel)!.NeedsSave)
+        {
+            var result = MessageBoxManager.GetMessageBoxStandard(
+                new MessageBoxStandardParams()
+                {
+                    ButtonDefinitions = ButtonEnum.YesNo,
+                    ContentTitle = "Discard Changes",
+                    ContentMessage = "You are about to discard unsaved changes. Continue?",
+                    WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                }).ShowWindowDialogAsync(this);
+            if (result.Result == ButtonResult.No)
+            {
+                return;
+            }
+        }
+        var selection = await StorageProvider.OpenFolderPickerAsync(
+                new()
+                {
+                    Title = "Select the root folder of your mod",
+                    AllowMultiple = false,
+                }
+            );
+        if (selection.Count != 1)
+        {
+            return;
+        }
+        string folder = Uri.UnescapeDataString(selection[0].Path.AbsolutePath);
+        if (!string.IsNullOrEmpty(folder))
+        {
+            (DataContext as MainWindowViewModel)!.OpenMod(folder);
+        }
+    }
+
+    private void Save_Click(object? sender, RoutedEventArgs e)
     {
         (DataContext as MainWindowViewModel)!.Save();
+    }
+
+    private async void Settings_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        await new SettingsWindow().ShowDialog(this);
     }
 }
