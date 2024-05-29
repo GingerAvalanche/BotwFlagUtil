04ed210f88219c6fec8ed02256cb9e4b5a0668f9
GingerAvalanche <chodness@gmail.com>
2024-05-28 22:17:26 -0700

    Update Settings for FlagUtil

--- BotwFlagUtil/Models/Settings.cs
+++ BotwFlagUtil/Models/Settings.cs
@@ -1,3 +1,4 @@
+using SharpYaml.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,37 +7,36 @@ using System.Text.Json.Serialization;
 
 namespace BotwFlagUtil
 {
-    internal class Settings(string game, string update, string dlc, string gameNx, string dlcNx)
+    internal class Settings(string gameDir, string updateDir, string dlcDir, string gameDirNx, string dlcDirNx)
     {
-        [JsonInclude]
+        private static readonly string settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "botw_tools", "settings.json");
         public bool WiiU = true;
         [JsonInclude]
-        public string gameDir = game;
+        public string gameDir = gameDir;
         [JsonInclude]
-        public string gameDirNx = gameNx;
+        public string gameDirNx = gameDirNx;
         [JsonInclude]
-        public string updateDir = update;
+        public string updateDir = updateDir;
         [JsonInclude]
-        public string dlcDir = dlc;
+        public string dlcDir = dlcDir;
         [JsonInclude]
-        public string dlcDirNx = dlcNx;
+        public string dlcDirNx = dlcDirNx;
 
         public static Settings Load()
         {
             Settings value;
             string appdata = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            if (File.Exists(Path.Combine(appdata, "botw_tools", "settings.json")))
+            if (File.Exists(settingsPath))
             {
                 value = JsonSerializer.Deserialize<Settings>(
-                    File.ReadAllText(Path.Combine(appdata, "botw_tools", "settings.json"))
+                    File.ReadAllText(settingsPath)
                 )!;
                 if (Validate(value))
                 {
                     return value;
                 }
             }
-
-            if (File.Exists(Path.Combine(appdata, "bcml", "settings.json")))
+            else if (File.Exists(Path.Combine(appdata, "bcml", "settings.json")))
             {
                 Dictionary<string, dynamic> bcmlSettings =
                     JsonSerializer.Deserialize<Dictionary<string, dynamic>>(
@@ -49,59 +49,36 @@ namespace BotwFlagUtil
                     bcmlSettings["game_dir_nx"],
                     bcmlSettings["dlc_dir_nx"]
                 );
-                if (Validate(value))
-                {
-                    Directory.CreateDirectory(Path.Combine(appdata, "botw_tools"));
-                    File.WriteAllText(
-                        Path.Combine(appdata, "botw_tools", "settings.json"),
-                        JsonSerializer.Serialize(
-                            value,
-                            typeof(Settings),
-                            Helpers.jsOpt
-                        )
-                    );
-                    return value;
-                }
             }
-            do
+            else
             {
-                Console.WriteLine("Dump paths not found/validated. You will now be asked for your dump locations. Press enter after reading each line to continue.");
-                Console.ReadKey();
-                Console.WriteLine("Please enter the following dump locations for WiiU. If you don't have a WiiU dump, simply press enter to skip to Switch.");
-                Console.Write("Game dump (ends in 'content'):");
-                string game = Console.ReadLine()!;
-                Console.Write("Update dump (ends in 'content'):");
-                string update = Console.ReadLine()!;
-                Console.Write("DLC dump (ends in '0010'):");
-                string dlc = Console.ReadLine()!;
-                Console.WriteLine("Please enter the following dump locations for Switch. If you don't have a Switch dump, simply press enter to skip.");
-                Console.Write("Game dump (ends in '01007EF00011E800/romfs'):");
-                string gameNx = Console.ReadLine()!;
-                Console.Write("DLC dump (ends in '01007EF00011F001/romfs'):");
-                string dlcNx = Console.ReadLine()!;
+                value = new(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
+            }
 
-                value = new(game, update, dlc, gameNx, dlcNx);
-            } while (!Validate(value));
-
-            if (!Directory.Exists(Path.Combine(appdata, "botw_tools")))
+            if (!Directory.Exists(Directory.GetParent(settingsPath)!.FullName))
             {
-                Directory.CreateDirectory(Path.Combine(Path.Combine(appdata, "botw_tools")));
+                Directory.CreateDirectory(Directory.GetParent(settingsPath)!.FullName);
             }
             File.WriteAllText(
-                Path.Combine(appdata, "botw_tools", "settings.json"),
-                JsonSerializer.Serialize(value, typeof(Settings), Helpers.jsOpt)
+                settingsPath,
+                JsonSerializer.Serialize(value, Helpers.jsOpt)
             );
             return value;
         }
 
+        public void Save()
+        {
+            File.WriteAllText(settingsPath, JsonSerializer.Serialize(this, Helpers.jsOpt));
+        }
+
         public static bool Validate(Settings value)
         {
-            if (value == null || (value.gameDir == "" && value.gameDirNx == ""))
+            if (value == null || (value.gameDir == string.Empty && value.gameDirNx == string.Empty))
             {
                 Console.WriteLine("Must have game dumps for at least one of the consoles.");
                 return false;
             }
-            if (value.gameDir != "")
+            if (value.gameDir != string.Empty)
             {
                 if (!File.Exists(Path.Combine(value.gameDir, "Pack", "Dungeon000.pack")))
                 {
@@ -114,20 +91,20 @@ namespace BotwFlagUtil
                     Console.WriteLine("WiiU update dump failed to validate.");
                     return false;
                 }
-                if (!File.Exists(Path.Combine(value.dlcDir, "Pack", "AocMainField.pack")))
+                if (value.dlcDir != string.Empty && !File.Exists(Path.Combine(value.dlcDir, "Pack", "AocMainField.pack")))
                 {
                     Console.WriteLine("WiiU DLC dump failed to validate.");
                     return false;
                 }
             }
-            if (value.gameDirNx != "")
+            if (value.gameDirNx != string.Empty)
             {
                 if (!File.Exists(Path.Combine(value.gameDirNx, "Pack", "Dungeon000.pack")))
                 {
                     Console.WriteLine("Switch game dump failed to validate.");
                     return false;
                 }
-                if (!File.Exists(Path.Combine(value.dlcDirNx, "Pack", "AocMainField.pack")))
+                if (value.dlcDirNx != string.Empty && !File.Exists(Path.Combine(value.dlcDirNx, "Pack", "AocMainField.pack")))
                 {
                     Console.WriteLine("Switch DLC dump failed to validate.");
                     return false;
@@ -136,5 +113,11 @@ namespace BotwFlagUtil
 
             return true;
         }
+
+        public static bool ValidateGameDir(string path) => File.Exists(Path.Combine(path, "Pack", "Dungeon000.pack"));
+        public static bool ValidateUpdateDir(string path) => File.Exists(Path.Combine(path, "Actor", "Pack", "FldObj_MountainSnow_A_M_02.sbactorpack"));
+        public static bool ValidateDlcDir(string path) => File.Exists(Path.Combine(path, "Pack", "AocMainField.pack"));
+        public static bool ValidateGameDirNx(string path) => ValidateGameDir(path) & ValidateUpdateDir(path);
+        public static bool ValidateDlcDirNx(string path) => ValidateDlcDir(path);
     }
 }
