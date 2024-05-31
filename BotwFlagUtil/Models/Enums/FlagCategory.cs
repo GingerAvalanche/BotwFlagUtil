460e443a20c24a22894bd0a64fac5e9960c61903
Ginger Chody <chodness@gmail.com>
2024-05-31 12:02:50 -0700

    Add FlagCategory enum for bool flag categories

---
+++ BotwFlagUtil/Models/Enums/FlagCategory.cs
@@ -0,0 +1,16 @@
+namespace BotwFlagUtil.Enums
+{
+    public enum FlagCategory : int
+    {
+        Clear = 1,
+        Animal = 2,
+        Enemy = 3,
+        Sozai = 4,
+        Weapon = 5,
+        Other = 6,
+        Boss = 7,
+        Korok = 11,
+        DLC,
+        HardMode,
+    }
+}
